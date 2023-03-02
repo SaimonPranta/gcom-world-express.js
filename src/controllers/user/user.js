591a445e2cf713243940645b99cd896db0960286
pranta <saimonpranta@gmail.com>
2023-03-02 13:55:57 +0600

    added /user/registaion/data_verification route for regtistation from data verificaton

--- src/controllers/user/user.js
+++ src/controllers/user/user.js
@@ -4,23 +4,25 @@ exports.user = async (req, res) => {
   try {
   } catch (error) {}
 };
-exports.registation = async (req, res) => {
-const {
-    fullName,
-    fatherName,
-    motherName,
-    gender,
-    phoneNumber,
-    userID,
-    email,
-    address,
-    country,
-    nid,
-    referID,
-    password,
-  } = req.body;
 
+exports.registationExistingInfoVerification = async (req, res) => {
+  console.log("ok");
   try {
+    const {
+      fullName,
+      fatherName,
+      motherName,
+      gender,
+      phoneNumber,
+      userID,
+      email,
+      address,
+      country,
+      nid,
+      referID,
+      placementID,
+      password,
+    } = req.body;
     if (
       !fullName ||
       !fatherName ||
@@ -35,23 +37,82 @@ const {
       !referID ||
       !password
     ) {
-      console.log("data are missing")
+      console.log("data are missing");
       return res.status(200).json({});
     }
-    const checkDataIsExist = await userCollection.findOne({})
+    const checkisUserIDExist = await userCollection.findOne({
+      userID,
+    });
+    const checkisphoneNumberExist = await userCollection.findOne({
+      phoneNumber,
+    });
+     const checkIsRefferIDExist = await userCollection.findOne({
+       userID: referID,
+     });
+      const checkIsPlacementIDExist = await userCollection.findOne({
+        userID: placementID,
+      });
+
+    if (checkisUserIDExist?._id) {
+      return res.status(200).json({
+        failed: "Sorry, Your provided User ID is already exist.",
+      });
+    }
+    if (checkisphoneNumberExist?._id) {
+      return res.status(200).json({
+        failed: "Sorry, Your provided Phone Number is already exist.",
+      });
+    }
+    if (!checkIsRefferIDExist) {
+      return res.status(200).json({
+        failed:
+          "Please provide correct Refferal ID, Your provided Refferal ID is not exist.",
+      });
+    }
+    if (!checkIsPlacementIDExist) {
+      return res.status(200).json({
+        failed:
+          "Please provide correct Placement ID, Your provided Placement ID is not exist.",
+      });
+    }
+    return res.status(200).json({
+      sucess: true,
+    });
+  } catch (error) {
+    console.log(error)
+    return res.status(200).json({
+      failed: "Something is wrong please try again letter.",
+    });
+  }
+};
+exports.registation = async (req, res) => {
+  try {
+    const {
+      fullName,
+      fatherName,
+      motherName,
+      gender,
+      phoneNumber,
+      userID,
+      email,
+      address,
+      country,
+      nid,
+      referID,
+      password,
+    } = req.body;
+    console.log("hi");
 
     const processData = await new userCollection({
       ...req.body,
       placementID: req.body.referID,
     });
     const data = await processData.save();
-      console.log("data are ok", data);
-      return res.status(200).json({});
-
-
+    console.log("data are ok", data);
+    return res.status(200).json({});
   } catch (error) {
     console.log(error.message);
-
+    return res.status(200).json({});
   }
 };
 exports.login = async (req, res) => {
--- src/routes/user/user.js
+++ src/routes/user/user.js
@@ -1,13 +1,22 @@
 const express = require("express");
 const router = express.Router();
-const { user, registation, login, updateUser, deleteUser } = require("../../controllers/user/user");
+const {
+  user,
+  registation,
+  login,
+  updateUser,
+  deleteUser,
+  registationExistingInfoVerification,
+} = require("../../controllers/user/user");
 
 router.get("/:id", user);
+router.post(
+  "/registation/data_verification",
+  registationExistingInfoVerification
+);
 router.post("/registation", registation);
 router.post("/login", login);
 router.patch("/update/:id", updateUser);
 router.delete("/delete/:id", deleteUser);
-
-
 
 module.exports = router;
